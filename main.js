580a7fcd44525863a1e08602edce2437f504b852
Thiyobuilds <146916604+Thiyobuilds@users.noreply.github.com>
2024-01-14 02:19:31 -0200

    main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ submit.addEventListener("submit", (e) => {
 });
 
 btn.addEventListener("click", () => {
-  setTimeout(emptyInput, 5000);
+  setTimeout(emptyInput, 2000);
   function emptyInput() {
     inputs.forEach((input) => (input.value = ""));
     textarea.forEach((textarea) => (textarea.value = ""));
